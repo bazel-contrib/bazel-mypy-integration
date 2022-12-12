03f423fc746da6a0c7fadaa0ad5436a63afad1c1
Mark Elliot <mark.elliot@gmail.com>
2022-12-11 19:19:18 -0500

    Handle mypy versions >= 0.981

--- mypy/main.py
+++ mypy/main.py
@@ -4,4 +4,7 @@ import sys
 from mypy.main import main
 
 if __name__ == '__main__':
-    main(None, sys.stdout, sys.stderr)
+    if main.__version__ and '0.981' <= main.__version__:
+        main()
+    else:
+        main(None, sys.stdout, sys.stderr)
