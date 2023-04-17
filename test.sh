265fc38b0ec4c14fa5362ca0295837d762f913a6
Casey <casey.duquette@snapchat.com>
2023-04-17 15:15:31 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,6 +6,8 @@ set -euo pipefail
 
 test_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/test/shell
 
+bazel clean --expunge
+
 echo "Running integration tests against repo's Bazel workspace..."
 # shellcheck source=/dev/null
 source "${test_dir}"/test_mypy.sh
