931234715654da3d13049355b0abdf9534238f41
Martin Medler <36563496+martis42@users.noreply.github.com>
2024-07-02 15:06:33 +0200

    Prepare next release (#131)
    * Remove compatibility_level from examples
    
    This is superfluous noise as we don't deploy the examples
    
    * Set module to default compatibility_level
    
    The default value is 0. While the next release will be a breaking change,
    imho the compatibility_level is counted starting with the first bzlmod
    release, which is still pending.
    The compatibility_level is remove completely, as the bazel-central-registry
    maintainers don't want modules to explicitly mention the default value.
    
    * Use rolling releases in bcr tests
    
    We should be aware of incompatibilities to upcoming Bazel versions as early
    as possible.
    We reorder the arguments as increasing version from left to right seems to
    be the dominant style in the bcr.
    
    * Extend bcr tests
    
    The tests in the examples directory ensure the bazelized mypy understands
    a lot of Python code examples. This is great, but is missing testing for
    expected failures. The test script in the root module does perform such
    tests.
    Ideally, there would be a directory with all relevant integration tests
    which uses the root module as external dependency. For now this largish
    refactoring is out of scope and thus we test both the root module itself
    and the examples.
    
    * Rework release notes template
    
    - Advertise bzlmod as preferred option due to WORKSPACE being legacy
    - Do not explain how to use bzlmod. The information is not sufficient to
      understand properly what bzlmod is about for somebody new to bzlmod.
      The information is superfluous for everybody who has read the bzlmod doc.
    - Fix WORKSPACE template to work with current HEAD

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -12,18 +12,17 @@ git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
 SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
 
 cat << EOF
-## Using Bzlmod with Bazel 6 or later
+## Using Bzlmod (Recommended for Bazel 6 or later)
 
-1. (Bazel 6 only) Enable with \`common --enable_bzlmod\` in \`.bazelrc\`.
-2. Add to your \`MODULE.bazel\` file:
+Add to your \`MODULE.bazel\` file:
 
 \`\`\`starlark
 bazel_dep(name = "mypy_integration", version = "${TAG:1}")
 \`\`\`
 
-## Using WORKSPACE
+## Using WORKSPACE (Legacy)
 
-Paste this snippet into your \`WORKSPACE.bazel\` file:
+Add to your \`WORKSPACE.bazel\` file:
 
 \`\`\`starlark
 load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
@@ -35,19 +34,8 @@ http_archive(
     url = "https://github.com/bazel-contrib/bazel-mypy-integration/releases/download/${TAG}/${ARCHIVE}",
 )
 
-load(
-    "@mypy_integration//repositories:repositories.bzl",
-    mypy_integration_repositories = "repositories",
-)
+load("@mypy_integration//repositories:repositories.bzl", mypy_integration_repositories = "repositories")
+
 mypy_integration_repositories()
-
-load("@mypy_integration//repositories:deps.bzl", mypy_integration_deps = "deps")
-
-mypy_integration_deps(
-    mypy_requirements_file="//tools/typing:mypy_version.txt",
-    # python_interpreter = "python3.9"  # $PATH is searched for exe.
-    # OR
-    # python_interpreter_target = "@python3_interpreter//:bin/python3",
-)
 \`\`\`
 EOF
